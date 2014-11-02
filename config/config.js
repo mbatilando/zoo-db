a1b9fda0682aa316c790c23dda4dcde745c2a350
Mari Batilando <mari.batilando@gmail.com>
2014-11-01 21:45:11 -0700

    Change DB settings
    Change DB settings
    
    Change DB settings
    
    Add console log
    
    Add console log
    
    Change development config
    
    Change port number
    
    Change port number
    
    Change to prod
    
    Fix config

--- config/config.js
+++ config/config.js
@@ -1,36 +1,36 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    env = process.env.NODE_ENV || 'development',
+    port = process.env.PORT || 3000;
 
 var config = {
   development: {
     root: rootPath,
     app: {
-      name: 'info257test'
+      name: 'Zoo DB Dev'
     },
     port: 3000,
     db: 'postgres://localhost/Mari'
-    
   },
 
   test: {
     root: rootPath,
     app: {
-      name: 'info257test'
+      name: 'Zoo DB Test'
     },
     port: 3000,
     db: 'postgres://localhost/info257test-test'
-    
   },
 
   production: {
     root: rootPath,
     app: {
-      name: 'info257test'
+      name: 'Zoo DB Prod'
     },
-    port: 3000,
-    db: 'postgres://localhost/info257test-production'
-    
+    port: port,
+    db: 'postgres://kupehbzbjzufdf:ck_UuGKW6aGE5CRiYcyLeAj8wM@ec2-54-235-250-41.compute-1.amazonaws.com:5432/d2lt4df5854guc',
+    username: 'kupehbzbjzufdf',
+    password: 'ck_UuGKW6aGE5CRiYcyLeAj8wM'
   }
 };
 
