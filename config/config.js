5971e14d0450cd5eec778a0c613c7f75a8da4a95
Mari Batilando <mari.batilando@gmail.com>
2014-11-01 21:48:41 -0700

    Change DB settings

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ var config = {
       name: 'Zoo DB Dev'
     },
     port: 3000,
-    db: 'postgres://localhost/Mari'
+    db: 'postgres://kupehbzbjzufdf:ck_UuGKW6aGE5CRiYcyLeAj8wM@ec2-54-235-250-41.compute-1.amazonaws.com:5432/d2lt4df5854guc'
     
   },
 
@@ -19,7 +19,7 @@ var config = {
       name: 'Zoo DB Test'
     },
     port: 3000,
-    db: 'postgres://localhost/info257test-test'
+    db: 'postgres://kupehbzbjzufdf:ck_UuGKW6aGE5CRiYcyLeAj8wM@ec2-54-235-250-41.compute-1.amazonaws.com:5432/d2lt4df5854guc'
     
   },
 
