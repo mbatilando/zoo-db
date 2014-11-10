a4657a4cf38e94e28047f5e7df852a18b1ae2387
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-11-09 18:34:09 -0800

    Added Zookeeper model.

---
+++ app/models/zookeeper.js
@@ -0,0 +1,17 @@
+module.exports = function (sequelize, DataTypes) {
+
+  var Zookeeper = sequelize.define('Zookeeper', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    work_days: DataTypes.ENUM('M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su'),
+    zoo: {
+        type: DataTypes.INTEGER,
+        references: 'Zoo',
+        referencesKey: 'id'
+    }
+  });
+
+  return Zookeeper;
+};
+
