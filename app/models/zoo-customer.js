5f024246843a9958696cb7826ac25f00cfdc2b5e
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-11-09 18:48:58 -0800

    Fixed 'Zoo Customer' name.

--- app/models/zoo-customer.js
+++ app/models/zoo-customer.js
@@ -1,6 +1,6 @@
 module.exports = function (sequelize, DataTypes) {
 
-  var ZooCustomer = sequelize.define('ZooCustomer', {
+  var ZooCustomer = sequelize.define('Zoo Customer', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     phone: DataTypes.STRING,
