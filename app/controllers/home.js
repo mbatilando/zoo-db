208c040c3b906411b67370f1a871fa9829b52c2b
Mari Batilando <mari.batilando@gmail.com>
2014-11-12 22:47:09 -0800

    Add dynamic rows to all views

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,13 +7,6 @@ module.exports = function (app) {
 };
 
 router.get('/', function (req, res, next) {
-
-  // db.Article.findAll().success(function (articles) {
-  //   res.render('index', {
-  //     title: 'Generator-Express MVC',
-  //     articles: articles
-  //   });
-  // });
   res.render('index', {});
 });
 
@@ -22,7 +15,9 @@ router.get('/add/customer', function (req, res, next) {
 });
 
 router.get('/view/customers', function (req, res, next) {
-  res.render('customer/view-customers', {});
+  db['Zoo Customer'].findAll().success(function (ZooCustomers) {
+    res.render('customer/view-customers', { ZooCustomers: ZooCustomers });
+  });
 });
 
 router.get('/add/zoo', function (req, res, next) {
@@ -30,7 +25,9 @@ router.get('/add/zoo', function (req, res, next) {
 });
 
 router.get('/view/zoos', function (req, res, next) {
-  res.render('zoo/view-zoos', {});
+  db['Zoo'].findAll().success(function (Zoos) {
+    res.render('zoo/view-zoos', { Zoos: Zoos });
+  });
 });
 
 router.get('/add/zookeeper', function (req, res, next) {
@@ -38,7 +35,9 @@ router.get('/add/zookeeper', function (req, res, next) {
 });
 
 router.get('/view/zookeepers', function (req, res, next) {
-  res.render('zookeeper/view-zookeepers', {});
+  db['Zookeeper'].findAll().success(function (Zookeepers) {
+    res.render('zookeeper/view-zookeepers', { Zookeepers: Zookeepers });
+  });
 });
 
 router.get('/add/show', function (req, res, next) {
@@ -46,7 +45,9 @@ router.get('/add/show', function (req, res, next) {
 });
 
 router.get('/view/shows', function (req, res, next) {
-  res.render('show/view-shows', {});
+  db['Show'].findAll().success(function (Shows) {
+    res.render('show/view-shows', { Shows: Shows });
+  });
 });
 
 router.get('/add/animal', function (req, res, next) {
@@ -54,7 +55,9 @@ router.get('/add/animal', function (req, res, next) {
 });
 
 router.get('/view/animals', function (req, res, next) {
-  res.render('animal/view-animals', {});
+  db['Animal'].findAll().success(function (Animals) {
+    res.render('animal/view-animals', { Animals: Animals });
+  });
 });
 
 router.get('/add/exhibit', function (req, res, next) {
@@ -62,7 +65,9 @@ router.get('/add/exhibit', function (req, res, next) {
 });
 
 router.get('/view/exhibits', function (req, res, next) {
-  res.render('exhibit/view-exhibits', {});
+  db['Exhibit'].findAll().success(function (Exhibits) {
+    res.render('exhibit/view-exhibits', { Exhibits: Exhibits });
+  });
 });
 
 router.get('/add/zoo-membership', function (req, res, next) {
@@ -70,12 +75,17 @@ router.get('/add/zoo-membership', function (req, res, next) {
 });
 
 router.get('/view/zoo-memberships', function (req, res, next) {
-  res.render('zoo-membership/view-zoo-memberships', {});
+  db['ZooMembership'].findAll().success(function (ZooMemberships) {
+    res.render('zoo-membership/view-zoo-memberships', { ZooMemberships: ZooMemberships });
+  });
 });
 
 router.get('/add/species', function (req, res, next) {
   res.render('species/add-species', {});
 });
+
 router.get('/view/species', function (req, res, next) {
-  res.render('species/view-species', {});
+  db['Species'].findAll().success(function (Species) {
+    res.render('species/view-species', { Species: Species });
+  });
 });
