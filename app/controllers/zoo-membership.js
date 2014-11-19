c29f703ade401e4d05f88529d08615f7502ca1a5
Mari Batilando <mari.batilando@gmail.com>
2014-11-18 22:41:54 -0800

    Add CRUD func for Zoo Membership

--- app/controllers/zoo-membership.js
+++ app/controllers/zoo-membership.js
@@ -17,7 +17,41 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-  db['ZooMembership'].findAll().success(function (ZooMemberships) {
+  db['ZooMembership'].findAll({ order: 'id ASC' }).success(function (ZooMemberships) {
     res.render('zoo-membership/view-zoo-memberships', { ZooMemberships: ZooMemberships });
+  });
+});
+
+router.post('/', function (req, res, next) {
+  db['ZooMembership'].create(req.body).success(function (zooMembership) {
+    db['ZooMembership'].findAll().success(function (zooMemberships) {
+      req.dataProcessed = zooMemberships;
+      viewZooMemberships(req, res);
+    });
+  });
+});
+
+function viewZooMemberships (req, res) {
+  var context = req.dataProcessed;
+  res.render('zoo-membership/view-zoo-memberships', { ZooMemberships: context });
+}
+
+router.put('/:id', function (req, res, next) {
+  db['ZooMembership'].find({ where: { id: req.params.id }}).success(function (zooMembership) {
+    zooMembership.updateAttributes(req.body).success(function (zooMemberships) {
+      db['ZooMembership'].findAll().success(function (zooMemberships) {
+        req.dataProcessed = zooMemberships;
+        viewZooMemberships(req, res);
+      });
+    });
+  });
+});
+
+router.delete('/:id', function (req, res, next) {
+  db['ZooMembership'].find({ where: { id: req.params.id }}).success(function (zooMemberships) {
+    zooMemberships.destroy().success(function () {
+      res.send(200);
+      viewZooMemberships(req, res);
+    });
   });
 });
