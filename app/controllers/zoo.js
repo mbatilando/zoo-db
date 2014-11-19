2fb948490ea1eee0837bff94c79a6737dd868187
Mari Batilando <mari.batilando@gmail.com>
2014-11-18 23:01:19 -0800

    Add CRUD func for Zookeeper

--- app/controllers/zoo.js
+++ app/controllers/zoo.js
@@ -17,7 +17,41 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-  db['Zoo'].findAll().success(function (Zoos) {
+  db['Zoo'].findAll({ order: 'id ASC' }).success(function (Zoos) {
     res.render('zoo/view-zoos', { Zoos: Zoos });
+  });
+});
+
+router.post('/', function (req, res, next) {
+  db['Zoo'].create(req.body).success(function (zoo) {
+    db['Zoo'].findAll().success(function (zoos) {
+      req.dataProcessed = zoos;
+      viewZoos(req, res);
+    });
+  });
+});
+
+function viewZoos (req, res) {
+  var context = req.dataProcessed;
+  res.render('zoo/view-zoos', { Zoos: context });
+}
+
+router.put('/:id', function (req, res, next) {
+  db['Zoo'].find({ where: { id: req.params.id }}).success(function (zoo) {
+    zoo.updateAttributes(req.body).success(function (zoos) {
+      db['Zoo'].findAll().success(function (zoos) {
+        req.dataProcessed = zoos;
+        viewZoos(req, res);
+      });
+    });
+  });
+});
+
+router.delete('/:id', function (req, res, next) {
+  db['Zoo'].find({ where: { id: req.params.id }}).success(function (zoos) {
+    zoos.destroy().success(function () {
+      res.send(200);
+      viewZoos(req, res);
+    });
   });
 });
--- app/controllers/zookeeper.js
+++ app/controllers/zookeeper.js
@@ -17,7 +17,41 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-  db['Zookeeper'].findAll().success(function (Zookeepers) {
+  db['Zookeeper'].findAll({ order: 'id ASC' }).success(function (Zookeepers) {
     res.render('zookeeper/view-zookeepers', { Zookeepers: Zookeepers });
+  });
+});
+
+router.post('/', function (req, res, next) {
+  db['Zookeeper'].create(req.body).success(function (zookeeper) {
+    db['Zookeeper'].findAll().success(function (zookeepers) {
+      req.dataProcessed = zookeepers;
+      viewZookeepers(req, res);
+    });
+  });
+});
+
+function viewZookeepers (req, res) {
+  var context = req.dataProcessed;
+  res.render('zookeeper/view-zookeepers', { Zookeepers: context });
+}
+
+router.put('/:id', function (req, res, next) {
+  db['Zookeeper'].find({ where: { id: req.params.id }}).success(function (zookeeper) {
+    zookeeper.updateAttributes(req.body).success(function (zookeepers) {
+      db['Zookeeper'].findAll().success(function (zookeepers) {
+        req.dataProcessed = zookeepers;
+        viewZookeepers(req, res);
+      });
+    });
+  });
+});
+
+router.delete('/:id', function (req, res, next) {
+  db['Zookeeper'].find({ where: { id: req.params.id }}).success(function (zookeepers) {
+    zookeepers.destroy().success(function () {
+      res.send(200);
+      viewZookeepers(req, res);
+    });
   });
 });
