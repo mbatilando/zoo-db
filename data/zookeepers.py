69ca7e0778d200b761d93e1b20d7351bc794c072
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-12-13 17:13:29 -0800

    Added scraper/generator and data for zookeepers but some fields are missing.

---
+++ data/zookeepers.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+from random import randint
+from urllib import urlopen
+
+FIRST_NAME = "first_name"
+LAST_NAME = "last_name"
+
+def get_zookeeper_data(url):
+  soup = BeautifulSoup(urlopen(url).read())
+  attrs = {"class": "CenturyGothic-Bold"}
+  return [str(span.text) for span in soup.findAll("span", attrs)[1:]]
+
+def get_zookeeper_names(url):
+  names = []
+  for name in get_zookeeper_data(url):
+    if "," in name:
+      name = name[:name.index(",")]
+    split_names = name.split()
+    names.append({FIRST_NAME: split_names[0], LAST_NAME: split_names[-1]})
+  return names
+
+def random_digits(length):
+  result = ""
+  for i in range(length):
+    result += str(randint(0, 9))
+  return result
+
+def random_phone_number():
+  return "(%s) %s-%s" % (random_digits(3), random_digits(3), random_digits(4))
+
+url = "http://zoo.sandiegozoo.org/content/key-leaders"
+content = "INSERT INTO \"Zookeepers\" (id, first_name, last_name, phone_number) VALUES\n"
+key = 1000
+
+for name in get_zookeeper_names(url):
+  content += ("  (%d, '%s', '%s', '%s'),\n" %
+      (key, name[FIRST_NAME], name[LAST_NAME], random_phone_number()))
+  key += 1
+
+content = content[:-2] + ";\n"
+
+sql_file = open("zookeepers.sql", "w")
+sql_file.write(content)
+sql_file.close()
+
---
+++ data/zookeepers.sql
@@ -0,0 +1,13 @@
+INSERT INTO "Zookeepers" (id, first_name, last_name, phone_number) VALUES
+  (1000, 'Douglas', 'Myers', '(828) 147-4868'),
+  (1001, 'Matthew', 'Musella', '(600) 848-2274'),
+  (1002, 'Paula', 'Brock', '(199) 133-5894'),
+  (1003, 'Mark', 'Stuart', '(970) 758-8856'),
+  (1004, 'Dwight', 'Scott', '(273) 807-5522'),
+  (1005, 'Robert', 'McClure', '(331) 144-9954'),
+  (1006, 'Allison', 'Alberts', '(805) 501-6221'),
+  (1007, 'Robert', 'Wiese', '(957) 977-0286'),
+  (1008, 'Donald', 'Janssen', '(595) 500-2746'),
+  (1009, 'Ted', 'Molter', '(824) 802-0928'),
+  (1010, 'Tim', 'Mulligan', '(569) 939-1545'),
+  (1011, 'Robert', 'Erhardt', '(835) 632-0205');
