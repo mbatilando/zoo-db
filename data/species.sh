511f62aafe1ff741a502535be5ae90e3faf530b7
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-12-14 18:00:21 -0800

    Last hacky bug fixes for species stuff.

--- data/species.sh
+++ data/species.sh
@@ -2,7 +2,10 @@ FILE=species.sql
 
 sed "s/[“”]/\"/g" $FILE > temp1
 sed "s/[‘’]/''/g" temp1 > temp2
-sed "s/—/ - /g" temp2 > $FILE
+sed "s/––/ -/g" temp2 > temp1
+sed "s/Przewalski's Horse/Przewalski''s Horse/g" temp1 > temp2
+sed "s/Steller's Sea-Eagle/Steller''s Sea-Eagle/g" temp2 > temp1
+sed "s/—/ - /g" temp1 > $FILE
 
 rm temp*
 
