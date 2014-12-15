1915de0a51aa78657dc844b1997432af5becd681
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-12-14 21:40:49 -0800

    Added shows data.

---
+++ data/shows.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Shows" (id, name, time, "ExhibitId") VALUES
+  (1000, 'Early Morning with Pandas', '08:30:00', 1004),
+  (1001, 'Breakfast with Koalas', '08:00:00', 1007),
+  (1002, 'Camp Critters Sea Lion Encounter', '13:00:00', 1002),
+  (1003, 'Dr. Zoolittle Explores Australia', '15:30:00', 1007);
