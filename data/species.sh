2871c107431dcd72c3dff8e1019a22c77ceae496
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2014-12-14 18:03:32 -0800

    Replaced '...'.

--- data/species.sh
+++ data/species.sh
@@ -5,7 +5,8 @@ sed "s/[‘’]/''/g" temp1 > temp2
 sed "s/––/ -/g" temp2 > temp1
 sed "s/Przewalski's Horse/Przewalski''s Horse/g" temp1 > temp2
 sed "s/Steller's Sea-Eagle/Steller''s Sea-Eagle/g" temp2 > temp1
-sed "s/—/ - /g" temp1 > $FILE
+sed "s/—/ - /g" temp1 > temp2
+sed "s/…/.../g" temp2 > $FILE
 
 rm temp*
 
