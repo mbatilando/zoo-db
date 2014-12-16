ebac3dbfbec14cf260282d73c3fc6f71df2abbec
Mari Batilando <mari.batilando@gmail.com>
2014-12-16 09:19:56 -0800

    idc

--- public/js/admin-dashboard.js
+++ public/js/admin-dashboard.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
 	var diagonal = d3.svg.diagonal()
 	.projection(function(d) { return [d.y, d.x]; });
 
-	var svg = d3.select("body").append("svg")
+	var svg = d3.select(".graph-container").append("svg")
 	.attr("width", width + margin.left + margin.right)
 	.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
