c1f140cd879fc80644f153eae189ef1b4873b2cf
Mari Batilando <mari.batilando@gmail.com>
2014-12-16 02:22:42 -0800

    Add redir to root

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,6 +6,14 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
+function authenticate (req) {
+  if (!req.session.username || !req.session.user_type) {
+    return false;
+  }
+  return true;
+}
+
 router.get('/', function (req, res, next) {
+	if (!authenticate(req)) return res.redirect('/authentication/login');
   res.render('index', {});
 });
