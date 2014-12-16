34d63ac29d7891851a1c85abc6c4a29ed958a99a
Mari Batilando <mari.batilando@gmail.com>
2014-12-16 07:51:56 -0800

    idc

--- app/controllers/admin-dashboard.js
+++ app/controllers/admin-dashboard.js
@@ -51,7 +51,10 @@ router.get('/api/:zooId', function (req, res, next) {
         // zoo.children[i].children = _.where(origResult, { last_name: zoo.children[i].last_name });
         for (var j = 0, jLen = origResult.length; j < jLen; j++) {
           if (zoo.children[i].last_name === origResult[j].last_name) {
-            zoo.children[i].children.push({ name: origResult[j].name, children: []})
+            // Check if exhibit hasn't been added yet
+            if (!_.contains(zoo.children[i].children, origResult[j].name)) {
+              zoo.children[i].children.push({ name: origResult[j].name, children: []})
+            }
           }
         }
       }
