25bd5bec7efac472267d24e3ee280a42c06ac303
Mari Batilando <mari.batilando@gmail.com>
2014-12-16 08:07:00 -0800

    idc

--- app/controllers/admin-dashboard.js
+++ app/controllers/admin-dashboard.js
@@ -57,10 +57,13 @@ router.get('/api/:zooId', function (req, res, next) {
             }
           }
         }
-        for (var k = 0, kLen = origResult.length; k < kLen; k++) {
-          if (zoo.children[i].children[k].name === origResult[k].name) {
-            if (_.findIndex(zoo.children[i].children[k].children, { name: origResult[k].common_name }) === -1) {
-              zoo.children[i].children[k].children.push({ name: origResult[k].common_name, children: []});
+
+        for (var j = 0, jLen = zoo.children[i].length; j < jLen; j++) {
+          for (var k = 0, kLen = origResult.length; k < kLen; k++) {
+            if (zoo.children[i].children[j].name === origResult[k].name) {
+              if (_.findIndex(zoo.children[i].children[j].children, { name: origResult[k].common_name }) === -1) {
+                zoo.children[i].children[j].children.push({ name: origResult[k].common_name, children: []});
+              }
             }
           }
         }
