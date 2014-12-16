c50044312f1903a18195169b061e92176b445ecb
Mari Batilando <mari.batilando@gmail.com>
2014-12-16 04:57:08 -0800

    Add async

--- app/controllers/admin-dashboard.js
+++ app/controllers/admin-dashboard.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     router = express.Router(),
-    db = require('../models');
+    db = require('../models'),
+    async = require('async');
 
 // function authenticate (req) {
 //   if (!req.session.username || !req.session.user_type) {
@@ -20,7 +21,54 @@ function authenticate (req) {
   return true;
 }
 
-router.get('/', function (req, res, next) {
+router.get('/:zooId', function (req, res, next) {
   // if (!authenticate(req)) return res.redirect('/authentication/login');
-  res.render('admin-dashboard/dashboard', { user: req.session.username });
+  var values = {};
+  async.series([
+    function (callback) {
+      db.sequelize
+          .query('SELECT COUNT(*) FROM "ZooMemberships" WHERE "ZooMemberships"."ZooId" = 1;')
+          .success(function (result) {
+            values.numZooMemberships = result[0].count;
+            callback();
+          })
+    },
+    function (callback) {
+      db.sequelize
+          .query('SELECT COUNT(*) FROM "Zookeepers" WHERE "Zookeepers"."ZooId" = 1;')
+          .success(function (result) {
+            console.log(result);
+            values.numZooKeepers = result[0].count;
+            callback();
+          })
+    },
+    function (callback) {
+      db.sequelize
+          .query('SELECT COUNT(*) FROM "Zookeepers", "Exhibits" WHERE "Zookeepers"."ZooId" = 1 AND "Zookeepers".id = "Exhibits"."ZookeeperId";')
+          .success(function (result) {
+            console.log(result);
+            values.numExhibits = result[0].count;
+            callback();
+          })
+    },
+    function (callback) {
+      db.sequelize
+          .query('SELECT COUNT(*) FROM "Zookeepers", "Exhibits", "Animals" WHERE "Zookeepers"."ZooId" = 1 AND "Zookeepers".id = "Exhibits"."ZookeeperId" AND "Exhibits".id = "Animals"."ExhibitId";')
+          .success(function (result) {
+            console.log(result);
+            values.numAnimals = result[0].count;
+            callback();
+          })
+    },
+    function (callback) {
+      db.sequelize
+          .query('SELECT COUNT(*) FROM (SELECT COUNT(*) FROM "Zookeepers", "Exhibits", "Animals" WHERE "Zookeepers"."ZooId" = 1 AND "Zookeepers".id = "Exhibits"."ZookeeperId" AND "Exhibits".id = "Animals"."ExhibitId" GROUP BY "Animals"."SpeciesId") AS subquery;')
+          .success(function (result) {
+            values.numSpecies = result[0].count;
+            console.log(values);
+            res.render('admin-dashboard/dashboard', { user: req.session.username, values: values });
+            callback();
+          })
+    }
+  ]);
 });
